dda31b4467d7d03f5609d46c599c9d813e09ce6b
Errol Pais <epais@thoughtworks.com>
2013-08-06 21:16:40 -0700

    Removed calcuation of distance between user and Food item from service

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,15 +2,15 @@ class SearchController < ApplicationController
   @@key = 'AIzaSyDoAwfKcmfVf8cvR_36X7s4D7AKWovmYzc'
   
   def index
-    user_location = Location.new(params)
-
-    results = Food.find_by_name(params[:search_term])
+    user_location = Location.new(params)       
     
+    q = "%#{params[:search_term]}%"
+    results = Food.where("lower(name) LIKE ?", q.downcase)   
+        
     results = [results].flatten.compact
 
     results.each do |result|
       result.picture = "#{result.picture}"
-      result.distance_away = user_location.haversine_distance_from_point(result) if user_location.valid?
     end
 
     render json: results
