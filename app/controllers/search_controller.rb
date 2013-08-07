7a9f1fb5b8e4a85a50b7840b04a4b28047764236
Sudhindra Rao <sudhindra.r.rao@gmail.com>
2013-08-06 18:23:46 -0700

    fixed search to accept a search term

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,8 +4,9 @@ class SearchController < ApplicationController
   def index
     user_location = Location.new(params)
     
-    results = Food.all
-    
+    results = Food.find_by_name(params[:search_term])
+    results = [results].flatten.compact
+
     results.each do |result|
       result.picture = "#{result.picture}"
       result.distance_away = user_location.haversine_distance_from_point(result) if user_location.valid?
